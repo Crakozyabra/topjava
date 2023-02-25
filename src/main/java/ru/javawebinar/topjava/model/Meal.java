b52c15e7f6a13ceb930d75b6445fddd709afff02
Crakozyabra <mrcrakozyabra@gmail.com>
2023-02-25 22:46:42 +0300

    HW04 complete

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,20 +1,44 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Objects;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal WHERE id=:id AND user.id=:user_id"),
+        @NamedQuery(name = Meal.GET, query = "FROM Meal WHERE id=:id AND user.id=:user_id"),
+        @NamedQuery(name = Meal.GET_ALL, query = "FROM Meal m JOIN FETCH m.user u WHERE u.id=:user_id ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.GET_ALL_FILTERED, query = "FROM Meal m JOIN FETCH m.user u WHERE m.dateTime >= :startDateTime AND m.dateTime < :endDateTime AND u.id=:user_id ORDER BY m.dateTime DESC")
+})
+@Entity
+@Table(name = "meal")
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.delete";
+    public static final String GET = "Meal.get";
+    public static final String GET_ALL = "Meal.getAll";
+    public static final String GET_ALL_FILTERED = "Meal.getAllFilter";
+
+    @NotNull
+    @Column(name = "date_time", nullable = false)
     private LocalDateTime dateTime;
 
+    @NotBlank
+    @Column(name = "description", nullable = false)
     private String description;
 
+    @Range
+    @Column(name = "calories", nullable = false)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Meal() {
