8211e6bc43cf3a042440d9dca1bec2de86f2c8de
Crakozyabra <mrcrakozyabra@gmail.com>
2023-03-09 16:41:00 +0300

    6_06_HW5_optional_fetch_join. Add bidirectional relationship between user and meal; methods getWithMeals, getWithUser and their tests

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.User;
@@ -45,5 +44,9 @@ public class UserService {
     public void update(User user) {
         Assert.notNull(user, "user must not be null");
         checkNotFoundWithId(repository.save(user), user.id());
+    }
+
+    public User getWithMeals(int id) {
+        return checkNotFoundWithId(repository.getWithMeals(id), id);
     }
 }
